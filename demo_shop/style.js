3095b14e98de4dea19e29f47a57ddc2dcd9fdcee
nguyenduythuan <thuanduy73h1@gmail.com>
2019-09-15 00:48:59 +0700

    xong giao dien cho desktop

--- demo_shop/style.js
+++ demo_shop/style.js
@@ -1,5 +1,3 @@
-var index = 0;
-var run;
 const img = document.getElementsByClassName("fade");
 const btDot = document.getElementsByClassName("dot");
 const btnLeft = document.getElementById("left");
@@ -8,6 +6,17 @@ const showSlide = document.getElementsByClassName("show-slide");
 const olwItem = document.getElementsByClassName("owl-item");
 const olwItemSP = document.getElementsByClassName("owl-item-sp");
 const slideSP = document.getElementsByClassName("slide-item");
+const toTop = document.getElementById("top");
+const btnL = document.getElementById("leftSP");
+const btnR = document.getElementById("rightSP");
+
+const numberSlide = slideSP[0].children.length;
+
+var index = 0;
+var run;
+var run2;
+var dem = 0;
+var numberTran;
 
 btnLeft.addEventListener("click", () => {
   clearTimeout(run);
@@ -19,7 +28,18 @@ btnRight.addEventListener("click", () => {
   runSlide(index < img.length ? (index += 0) : 0);
 });
 
+btnL.addEventListener("click", () => {
+  clearTimeout(run2);
+  runSlideSp(dem > 1 ? (dem += -2) : numberTran - 1);
+});
+
+btnR.addEventListener("click", () => {
+  clearTimeout(run2);
+  runSlideSp(dem < numberTran ? (dem += 0) : 0);
+});
+
 runSlide(index);
+runSlideSp(dem);
 
 window.onresize = function() {
   clearTimeout(run);
@@ -27,34 +47,78 @@ window.onresize = function() {
   runSlide(index);
 };
 
+window.onscroll = function() {scrollFunction()};
+
+function runSlideSp(soSlide) {
+  var aka;
+  var param;
+  var size = window.innerWidth;
+  for (let i = 0; i < numberSlide; i++) {
+    if (size > 1200) {
+      aka = 240;
+      param = numberSlide - 5;
+    } else {
+      if (size > 1015) {
+        aka = size/4;
+        param = numberSlide - 4;
+      } else {
+        if (size > 615) {
+          aka = size/3;
+          param = numberSlide - 3;
+        } else {
+          if (size > 465) {
+            aka = size/2;
+            param = numberSlide - 2;
+          } else {
+            aka = size;
+            param = numberSlide;
+          }
+        }
+      }
+    }
+    olwItemSP[i].style.width = `${aka}px`;
+  }
+  soSlide++;
+  dem = soSlide;
+  if (soSlide > param+1) {
+    soSlide = 1;
+    s = 1000;
+  }
+  numberTran = param+1;
+  slideSP[0].style.width = `${aka * numberSlide}px`;
+  slideSP[0].style.transition = `all ${400}ms ease 0s`;
+  slideSP[0].style.transform = `translate(-${aka * (soSlide-1)}px, 0px)`;
+  run2 = setTimeout(runSlideSp.bind(null, soSlide), 3000);
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+toTop.addEventListener("click", topFunction);
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function runSlide(number) {
   var s = 400;
-  var aka;
-  const size = window.innerWidth;
   showSlide[0].style.display = "flex";
   showSlide[0].style.width = `${window.innerWidth * 6}px`;
   showSlide[0].style.transition = `all ${s * 2}ms ease 0s`;
-  slideSP[0].style.width = `${window.innerWidth * 3.75}px`;
-  slideSP[0].style.transition = `all ${s}ms ease 0s`;
   number++;
   index = number;
   if (number > img.length) {
     number = 1;
     s = 1000;
   }
-  for (let i = 0; i < olwItemSP.length; i++) {
-    if (size > 1215) {
-      olwItemSP[i].style.width = `${240}px`;
-      aka = 240;
-    }
-    if (size > 1215) {
-      olwItemSP[i].style.width = `${240}px`;
-      aka = 240;
-    }
-  }
   showSlide[0].style.transform = `translate(-${window.innerWidth *
     (number - 1)}px, 0px)`;
-  slideSP[0].style.transform = `translate(-${aka * (number - 1)}px, 0px)`;
   for (let i = 0; i < img.length; i++) {
     olwItem[i].style.width = `${window.innerWidth}px`;
     btDot[i].classList.remove("pick");
