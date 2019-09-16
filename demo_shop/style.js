6288c8fafdc5b7807b3c9fa7f95c8d0fa30aceba
nguyenduythuan <thuanduy73h1@gmail.com>
2019-09-17 01:23:00 +0700

    Xong

--- demo_shop/style.js
+++ demo_shop/style.js
@@ -9,6 +9,8 @@ const slideSP = document.getElementsByClassName("slide-item");
 const toTop = document.getElementById("top");
 const btnL = document.getElementById("leftSP");
 const btnR = document.getElementById("rightSP");
+const wrapSp = document.getElementsByClassName("wrap-sp");
+const mmm = document.getElementsByClassName("owl-outline");
 
 const numberSlide = slideSP[0].children.length;
 
@@ -40,54 +42,97 @@ btnR.addEventListener("click", () => {
 
 runSlide(index);
 runSlideSp(dem);
+setWidth();
 
 window.onresize = function() {
   clearTimeout(run);
-  showSlide[0].style.width = `${window.innerWidth * 6}px`;
+  showSlide[0].style.width = `${screen.availWidth * 6}px`;
   runSlide(index);
+  setWidth();
 };
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
-function runSlideSp(soSlide) {
+function setWidth() {
+  const si = mmm[0].clientWidth;
   var aka;
-  var param;
-  var size = window.innerWidth;
-  for (let i = 0; i < numberSlide; i++) {
-    if (size > 1200) {
-      aka = 240;
-      param = numberSlide - 5;
+  for (let k = 0; k < wrapSp.length; k++) {
+    const ctSP = wrapSp[k].children;
+    const size = wrapSp[k].clientWidth;
+    if (si >= 1200) {
+      aka = size / 5 - 15;
     } else {
-      if (size > 1015) {
-        aka = size/4;
-        param = numberSlide - 4;
+      if (si >= 992) {
+        aka = size / 4 - 15;
       } else {
-        if (size > 615) {
-          aka = size/3;
-          param = numberSlide - 3;
+        if (si >= 768) {
+          aka = size / 3 - 15;
         } else {
-          if (size > 465) {
-            aka = size/2;
-            param = numberSlide - 2;
+          if (si >= 480) {
+            aka = size / 2 - 15;
           } else {
-            aka = size;
-            param = numberSlide;
+            aka = si - 35;
           }
         }
       }
     }
+    for (let i = 0; i < ctSP.length; i++) {
+      ctSP[i].style.width = `${aka}px`;
+    }
+  }
+}
+
+function getWidth(value, size) {
+  var aka;
+  var param;
+  if (size >= 1200) {
+    aka = 240;
+    param = value - 5;
+  } else {
+    if (size >= 992) {
+      aka = size / 4;
+      param = value - 4;
+    } else {
+      if (size >= 768) {
+        aka = size / 3;
+        param = value - 3;
+      } else {
+        if (size >= 480) {
+          aka = size / 2;
+          param = value - 2;
+        } else {
+          aka = size;
+          param = value - 1;
+        }
+      }
+    }
+  }
+  return {
+    aka,
+    param
+  };
+}
+
+function runSlideSp(soSlide) {
+  var s = 400;
+  var size = mmm[0].clientWidth;
+  const values = getWidth(numberSlide, size);
+  const { param, aka } = values;
+  for (let i = 0; i < numberSlide; i++) {
     olwItemSP[i].style.width = `${aka}px`;
   }
   soSlide++;
   dem = soSlide;
-  if (soSlide > param+1) {
+  if (soSlide > param + 1) {
     soSlide = 1;
     s = 1000;
   }
-  numberTran = param+1;
+  numberTran = param + 1;
   slideSP[0].style.width = `${aka * numberSlide}px`;
-  slideSP[0].style.transition = `all ${400}ms ease 0s`;
-  slideSP[0].style.transform = `translate(-${aka * (soSlide-1)}px, 0px)`;
+  slideSP[0].style.transition = `all ${s}ms ease 0s`;
+  slideSP[0].style.transform = `translate(-${aka * (soSlide - 1)}px, 0px)`;
   run2 = setTimeout(runSlideSp.bind(null, soSlide), 3000);
 }
 
@@ -108,8 +153,9 @@ function topFunction() {
 
 function runSlide(number) {
   var s = 400;
+  var rong = window.innerWidth;
   showSlide[0].style.display = "flex";
-  showSlide[0].style.width = `${window.innerWidth * 6}px`;
+  showSlide[0].style.width = `${rong * 6}px`;
   showSlide[0].style.transition = `all ${s * 2}ms ease 0s`;
   number++;
   index = number;
@@ -117,10 +163,9 @@ function runSlide(number) {
     number = 1;
     s = 1000;
   }
-  showSlide[0].style.transform = `translate(-${window.innerWidth *
-    (number - 1)}px, 0px)`;
+  showSlide[0].style.transform = `translate(-${rong * (number - 1)}px, 0px)`;
   for (let i = 0; i < img.length; i++) {
-    olwItem[i].style.width = `${window.innerWidth}px`;
+    olwItem[i].style.width = `${rong}px`;
     btDot[i].classList.remove("pick");
     btDot[i].addEventListener("click", () => clickDot(i));
   }
